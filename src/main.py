5cd883a1c08b4f76a1925c06eff325ff2c5e487b
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2025-05-09 17:33:06 +0800

    feat(app): tested and works

--- src/main.py
+++ src/main.py
@@ -22,12 +22,12 @@ def save_routes_to_json(routes, filepath="routes.json"):
 async def main(url_map, filepath):
     try:
         results = await asyncio.gather(
-            alltrails.scrape_routes(url_map["alltrails"]["url"], url_map["alltrails"]["threshold"]),
+            # alltrails.scrape_routes(url_map["alltrails"]["url"], url_map["alltrails"]["threshold"]),
             greatruns.scrape_routes(url_map["greatruns"]["url"], url_map["greatruns"]["threshold"]),
-            outdooractive.scrape_routes(url_map["outdooractive"]["url"], url_map["outdooractive"]["threshold"]),
+            # outdooractive.scrape_routes(url_map["outdooractive"]["url"], url_map["outdooractive"]["threshold"]),
             plotaroute.scrape_routes(url_map["plotaroute"]["url"], url_map["plotaroute"]["threshold"]),
-            rungo.scrape_routes(url_map["rungo"]["url"], url_map["rungo"]["threshold"]),
-            wikiloc.scrape_routes(url_map["wikiloc"]["url"], url_map["wikiloc"]["threshold"]),
+            # rungo.scrape_routes(url_map["rungo"]["url"], url_map["rungo"]["threshold"]),
+            # wikiloc.scrape_routes(url_map["wikiloc"]["url"], url_map["wikiloc"]["threshold"]),
         )
         combined_results = [route for route_list in results for route in route_list] 
         save_routes_to_json(combined_results, filepath)
@@ -44,26 +44,26 @@ if __name__ == "__main__":
             "url": "https://www.plotaroute.com/search?keyword=singapore",
             "threshold": 100,
         },
-        "rungo": {
-            "url": "https://routes.rungoapp.com/routes?search=singapore",
-            "threshold": 100,
-        },
-        "alltrails": {
-            "url": "https://www.alltrails.com/singapore/trail-running",
-            "threshold": 100,
-        },
+        # "rungo": {
+        #     "url": "https://routes.rungoapp.com/routes?search=singapore",
+        #     "threshold": 100,
+        # },
+        # "alltrails": {
+        #     "url": "https://www.alltrails.com/singapore/trail-running",
+        #     "threshold": 100,
+        # },
         "greatruns": {
             "url": "https://greatruns.com/location/singapore/",
             "threshold": 100,
         },
-        "outdooractive": {
-            "url": "https://www.outdooractive.com/en/routes/#area=1019113&filter=r-fullyTranslatedLangus-en,r-openState-,sb-sortedBy-0&wt=Singapore%20(Country)%0A1019113&zc=11.,103.81943,1.35119",
-            "threshold": 100,
-        },
-        "wikiloc": {
-            "url": "https://www.wikiloc.com/trails/running/singapore",
-            "threshold": 100,
-        },
+        # "outdooractive": {
+        #     "url": "https://www.outdooractive.com/en/routes/#area=1019113&filter=r-fullyTranslatedLangus-en,r-openState-,sb-sortedBy-0&wt=Singapore%20(Country)%0A1019113&zc=11.,103.81943,1.35119",
+        #     "threshold": 100,
+        # },
+        # "wikiloc": {
+        #     "url": "https://www.wikiloc.com/trails/running/singapore",
+        #     "threshold": 100,
+        # },
     }
     filepath = "../data/routes.json"
     asyncio.run(main(url_map, filepath))
